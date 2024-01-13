6da750b46a7dbee2c3c09b40b078e40f7f2b87b2
Víctor Quirino <victorsouzaq@gmail.com>
2024-01-13 00:58:56 -0300

    feat-Atualização e exclusão de usuário implementadas

--- CleanArchitecture/CleanArchitecture.API/Controllers/UsersController.cs
+++ CleanArchitecture/CleanArchitecture.API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using CleanArchitecture.Application.UseCases.CreateUser;
+using CleanArchitecture.Application.UseCases.DeleteUser;
 using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Application.UseCases.UpdateUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +30,27 @@ public class UsersController : ControllerBase
         CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<UpdateUserResponse>> Update(Guid id, UpdateUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        if (id != request.Id) return BadRequest();
+
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid? id, CancellationToken cancellationToken)
+    {
+        if (id is null) return BadRequest();
+
+        var deleteUserRequest = new DeleteUserRequest(id.Value);
+
+        var response = await _mediator.Send(deleteUserRequest, cancellationToken);
         return Ok(response);
     }
 }
