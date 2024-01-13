7a939003a119fe050ea0262520e17ea5fa71d603
ricardo <ricardo.dev1991@gmail.com>
2024-01-13 17:03:20 -0300

    🐞 fix(Modificando o nome do método ConfiguringServicesApp().):

--- CleanArchitecture/Core/CleanArchitecture.Aplication/Services/ServicesExtensions.cs
+++ CleanArchitecture/Core/CleanArchitecture.Aplication/Services/ServicesExtensions.cs
@@ -2,9 +2,9 @@ using Microsoft.Extensions.DependencyInjection;
 using FluentValidation;
 using System.Reflection;
 
-namespace CleanArchitecture.Aplication.Services;
+namespace CleanArchitecture.Application.Services;
 public static class ServiceExtensions {
-     public static void ConfigureApplicationServices(this IServiceCollection services) {
+     public static void ConfigureApplicationApp(this IServiceCollection services) {
          services.AddAutoMapper(Assembly.GetExecutingAssembly());
          services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
          services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
