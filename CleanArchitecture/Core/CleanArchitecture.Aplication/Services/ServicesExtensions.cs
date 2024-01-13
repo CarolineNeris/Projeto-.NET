ae8708d3ff18a25576cff2acecfe444763e331d0
ricardo <ricardo.dev1991@gmail.com>
2024-01-13 17:42:54 -0300

    Criando a classe ValidatorBehavior para validação de entradas e adicionando ao Services o AddTransiente.

--- CleanArchitecture/Core/CleanArchitecture.Aplication/Services/ServicesExtensions.cs
+++ CleanArchitecture/Core/CleanArchitecture.Aplication/Services/ServicesExtensions.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using FluentValidation;
 using System.Reflection;
+using MediatR;
+using CleanArchitecture.Application.Shared.Behavior;
 
 namespace CleanArchitecture.Application.Services;
 public static class ServiceExtensions {
@@ -8,5 +10,6 @@ public static class ServiceExtensions {
          services.AddAutoMapper(Assembly.GetExecutingAssembly());
          services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
          services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+          services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
      }   
 }
