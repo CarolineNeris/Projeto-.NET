8a43f81ce014a46a63445cf60d5321867c306c9e
ricardo <ricardo.dev1991@gmail.com>
2024-01-13 00:16:58 -0300

    ✨ feat(Criando os arquivos básicos da pasta CreateUser usando mediator para abstrair validações, commands de entrada e saída de dados.):

---
+++ CleanArchitecture/Core/CleanArchitecture.Aplication/Shared/UseCases/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,6 @@
+using MediatR;
+
+namespace CleanArchitecture.Aplication.UseCases.CreateUser;
+    public sealed record CreateUserRequest(string Email, string Name) :
+                                          IRequest<CreateUserResponse>;
+
