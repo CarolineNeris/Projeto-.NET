1ac8be7d2ce19c0860cb7f20bc047f45624424b6
Caio Gonçalves <caio.goncalves.cic@gmail.com>
2024-01-14 14:38:14 -0300

    Implementação Video 3

---
+++ Core/Application/UseCases/CreateUser/CreateUserMapper.cs
@@ -0,0 +1,16 @@
+﻿using Application;
+using AutoMapper;
+using Domain.Entities;
+
+
+namespace CleanArchitecture.Application.UseCases.CreateUser
+{
+    public sealed class CreateUserMapper : Profile
+    {
+        public CreateUserMapper()
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, CreateUserResponse>();
+        }
+    }
+}
