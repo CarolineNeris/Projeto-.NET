09c7c5ffa6b722c4764831d2f65648811b1dcaaf
Caio Gonçalves <caio.goncalves.cic@gmail.com>
2024-01-22 22:13:01 -0300

    Criação da estrutura de pastas

---
+++ Domain/Entities/Log.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public class Log : BaseEntity
+{
+    public required string Tipo { get; set; }
+    public required string Descricao { get; set; }
+    public DateTimeOffset DataHoraEvento { get; set; }
+    public required string Entidade { get; set; }
+    public required int TuplaId { get; set; }
+}
