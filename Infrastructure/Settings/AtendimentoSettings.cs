d0fb0c64fd6b1abd3577169d9e23dd7c5c847f0b
Caio Gonçalves <caio.goncalves.cic@gmail.com>
2024-02-10 15:45:21 -0300

    Criação da camada Infra

---
+++ Infrastructure/Settings/AtendimentoSettings.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Settings;
+public class AtendimentoSettings : IEntityTypeConfiguration<Atendimento>
+{
+   public void Configure(EntityTypeBuilder<Atendimento> builder)
+   {
+      builder
+         .ToTable("Atendimentos")
+         .HasKey(m => m.Id);
+
+      builder
+         .HasOne(m => m.Medico)
+         .WithMany(m => m.Atendimentos)
+         .HasForeignKey(m => m.MedicoId);
+
+      builder
+         .HasOne(m => m.Paciente)
+         .WithMany(m => m.Atendimentos)
+         .HasForeignKey(m => m.PacienteId);
+   }
+}
---
+++ Infrastructure/Settings/MedicoSettings.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Settings;
+public class MedicoConfigurations : IEntityTypeConfiguration<Medico>
+{
+   public void Configure(EntityTypeBuilder<Medico> builder)
+   {
+
+      builder
+         .ToTable("Medicos")
+         .HasKey(m => m.Id);
+   }
+}
