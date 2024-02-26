11b9dfa099f44d30ad4917d19834db99569fa136
Carol <carolnerisfonseca@gmail.com>
2024-02-25 22:37:54 -0300

    Controller de atendimento na camada api

--- WebAPI/Controllers/AtendimentoController.cs
+++ WebAPI/Controllers/AtendimentoController.cs
@@ -1,6 +1,33 @@
-﻿namespace WebAPI.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Persistence.Interfaces;
+using Core.Entities;
+using Application.Services.Interfaces;
+using Application.ViewModels;
+using Application.InputModels;
 
-public class AtendimentoController
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class AtendimentoController : ControllerBase
 {
+   private readonly IAtendimentoService _atendimentoService;
+   public List<AtendimentoViewModel> Atendimentos => _atendimentoService.GetAll();
+   public AtendimentoController(IAtendimentoService service) => _atendimentoService = service;
+   [HttpGet("atendimentos")]
+   public IActionResult Get()
+   {
+      return Ok(Atendimentos);
+
+   }
+
+   [HttpPost("atendimento")]
+   public IActionResult Post([FromBody] NewAtendimentoInputModel atendimento)
+   {
+      _atendimentoService.Create(atendimento);
+      return CreatedAtAction(nameof(Get), atendimento);
+ 
+   }
+
 
 }
