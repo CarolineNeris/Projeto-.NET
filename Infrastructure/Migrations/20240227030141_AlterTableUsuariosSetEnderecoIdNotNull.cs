009c9df00e739a836433d3aa45e0e4083e591623
Víctor Quirino <victorsouzaq@gmail.com>
2024-02-27 00:48:13 -0300

    fix - Alterando propriedade Endereco na entidade Usuario para não nula

---
+++ Infrastructure/Migrations/20240227030141_AlterTableUsuariosSetEnderecoIdNotNull.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTableUsuariosSetEnderecoIdNotNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuarios_Enderecos_EnderecoId",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EnderecoId",
+                table: "Usuarios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuarios_Enderecos_EnderecoId",
+                table: "Usuarios",
+                column: "EnderecoId",
+                principalTable: "Enderecos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuarios_Enderecos_EnderecoId",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EnderecoId",
+                table: "Usuarios",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuarios_Enderecos_EnderecoId",
+                table: "Usuarios",
+                column: "EnderecoId",
+                principalTable: "Enderecos",
+                principalColumn: "Id");
+        }
+    }
+}
